5a2cd06e3998d6bb298da93260b1dbd523813c14
nelson <nelsonjuarezsandoval4@gmail.com>
2022-09-07 18:44:29 -0500

    create Skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillsAndCompetencesComponent } from './skills-and-competences/skills-and-competences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkExperienceComponent
+    WorkExperienceComponent,
+    SkillsAndCompetencesComponent
   ],
   imports: [
     BrowserModule
